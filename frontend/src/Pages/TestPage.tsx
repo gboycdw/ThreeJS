08730f811981e954ff522b3ca71507aed690738a
gomao <gboycdw@gmail.com>
2023-07-21 13:17:20 +0900

    [feat] 컨트롤박스 컴포넌트 분리, 큐빅 깨짐방지를 위한 화면 비율 고정

--- frontend/src/Pages/TestPage.tsx
+++ frontend/src/Pages/TestPage.tsx
@@ -1,22 +1,17 @@
 import React, { useState, useRef, useEffect } from "react";
 import * as THREE from "three";
+import ControllBox from "../Components/ControllBox";
 
 function TestPage() {
-  const mountRef = useRef<HTMLDivElement>(null);
+  //   const mountRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
   const [browserWidth, setBrouserWidth] = useState(1024);
   const [browserHeight, setBrouserHeight] = useState(768);
   const [cameraDepth, setCameraDepth] = useState(5);
 
   useEffect(() => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
+    const camera = new THREE.PerspectiveCamera(75, 1024 / 768, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({
       canvas: canvasRef.current ?? undefined,
     });
@@ -50,37 +45,12 @@ function TestPage() {
 
   return (
     <>
-      <div>
-        <div>Controller</div>
-        <div>
-          <a>화면 사이즈 조정</a>
-          <input
-            className="w-50px h-10px"
-            defaultValue={1024}
-            onChange={(e) => {
-              setBrouserWidth(parseInt(e.target.value));
-            }}
-          />
-          <input
-            className="w-50px h-10px"
-            defaultValue={768}
-            onChange={(e) => {
-              setBrouserHeight(parseInt(e.target.value));
-            }}
-          />
-        </div>
-        <div>
-          <a>카메라 원근 조정</a>
-          <input
-            className="w-50px h-10px"
-            defaultValue={5}
-            onChange={(e) => {
-              setCameraDepth(parseInt(e.target.value));
-            }}
-          />
-        </div>
-      </div>
-      <div ref={mountRef}>
+      <div style={{ width: "1024px", height: "768px" }}>
+        <ControllBox
+          setBrouserWidth={setBrouserWidth}
+          setBrouserHeight={setBrouserHeight}
+          setCameraDepth={setCameraDepth}
+        />
         <canvas ref={canvasRef} />
       </div>
     </>
