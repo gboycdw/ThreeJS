4e5d3d3d749d7969f782f2b0a77086ea12bc3cf6
gomao <gboycdw@gmail.com>
2023-07-24 17:41:29 +0900

    [feat] 동적으로 카메라 원근조정 가능하도록 변경 외 ... 추가사항) 마우스 클릭이벤트 적용을 위한 변수 선언

--- frontend/src/Pages/MovingCubic2.tsx
+++ frontend/src/Pages/MovingCubic2.tsx
@@ -1,11 +1,43 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect, MutableRefObject } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Box } from "@react-three/drei";
 import ControllBox from "../Components/ControllBox";
+import { OrbitControls } from "@react-three/drei";
 
-function RotatingCube() {
+interface RotatingProps {
+  cameraDepth: number;
+  setMouseDownClientX: React.Dispatch<React.SetStateAction<number>>;
+  setMouseDownClientY: React.Dispatch<React.SetStateAction<number>>;
+  setMouseUpClientX: React.Dispatch<React.SetStateAction<number>>;
+  setMouseUpClientY: React.Dispatch<React.SetStateAction<number>>;
+}
+
+function RotatingCube(props: RotatingProps) {
+  const {
+    setMouseDownClientX,
+    setMouseDownClientY,
+    setMouseUpClientX,
+    setMouseUpClientY,
+    cameraDepth,
+  } = props;
   const cubeRef = useRef<any>(null);
 
+  const onMouseDown = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setMouseDownClientX(e.clientX);
+    setMouseDownClientY(e.clientY);
+  };
+  const onMouseUp = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setMouseUpClientX(e.clientX);
+    setMouseUpClientY(e.clientY);
+  };
+  // // 마우스 휠 컨트롤 관련
+  // useEffect(() => {
+  //   if (cubeRef.current) {
+  //     cubeRef.current.rotation.x += "마우스ㄱㄱㄱㄱ";
+  //     cubeRef.current.rotation.y += "마우슥ㄱㄱㄱ";
+  //   }
+  // }, []);
+  // 자동회전 관련
   useFrame(() => {
     if (cubeRef.current) {
       cubeRef.current.rotation.x += 0.01;
@@ -17,7 +49,7 @@ function RotatingCube() {
     <Box
       ref={cubeRef}
       args={[1, 1, 1]}
-      position={[0, 0, 0]}
+      position={[0, 0, cameraDepth]}
       material-color="skyblue"
     />
   );
@@ -26,8 +58,11 @@ function RotatingCube() {
 function MovingCubic2() {
   const [browserWidth, setBrowserWidth] = useState(1024);
   const [browserHeight, setBrowserHeight] = useState(768);
-  const [cameraDepth, setCameraDepth] = useState(5);
-
+  const [cameraDepth, setCameraDepth] = useState<number>(0);
+  const [mouseDownClientX, setMouseDownClientX] = useState(0);
+  const [mouseDownClientY, setMouseDownClientY] = useState(0);
+  const [mouseUpClientX, setMouseUpClientX] = useState(0);
+  const [mouseUpClientY, setMouseUpClientY] = useState(0);
   return (
     <>
       <div style={{ width: "1024px", height: "768px" }}>
@@ -36,14 +71,19 @@ function MovingCubic2() {
           setBrouserHeight={setBrowserHeight}
           setCameraDepth={setCameraDepth}
         />
-        <div> ㄱㅁㅇ</div>
-        <Canvas
-          style={{ width: browserWidth, height: browserHeight }}
-          camera={{ position: [0, 0, cameraDepth], fov: 75 }}
-        >
+        <div>
+          {[mouseDownClientX, mouseDownClientY, mouseUpClientX, mouseUpClientY]}
+        </div>
+        <Canvas style={{ width: browserWidth, height: browserHeight }}>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <RotatingCube />
+          <RotatingCube
+            cameraDepth={cameraDepth}
+            setMouseDownClientX={setMouseDownClientX}
+            setMouseDownClientY={setMouseDownClientY}
+            setMouseUpClientX={setMouseUpClientX}
+            setMouseUpClientY={setMouseUpClientY}
+          />
         </Canvas>
       </div>
     </>
