63f28597b4b8629939bf0e0128fde7bbb622e9da
gomao <gboycdw@gmail.com>
2023-07-26 11:52:39 +0900

    [feat] 회전옵션 추가 외 변경사항 다수 1. 회전옵션 추가(자동/수동, 중복가능) 2. 수동회전 시 마우스 드래그로 방향선태 ㄱ가능 3. 광원효과 추가

--- frontend/src/Pages/MovingCubic2.tsx
+++ frontend/src/Pages/MovingCubic2.tsx
@@ -2,46 +2,52 @@ import React, { useState, useRef, useEffect, MutableRefObject } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Box } from "@react-three/drei";
 import ControllBox from "../Components/ControllBox";
+import Checkbox from "../Components/CheckBox";
 import { OrbitControls } from "@react-three/drei";
 
 interface RotatingProps {
   cameraDepth: number;
-  setMouseDownClientX: React.Dispatch<React.SetStateAction<number>>;
-  setMouseDownClientY: React.Dispatch<React.SetStateAction<number>>;
-  setMouseUpClientX: React.Dispatch<React.SetStateAction<number>>;
-  setMouseUpClientY: React.Dispatch<React.SetStateAction<number>>;
+  mouseDownClientX: number;
+  mouseDownClientY: number;
+  mouseUpClientX: number;
+  mouseUpClientY: number;
+  currentClientX: number;
+  currentClientY: number;
+  autoRotation: boolean;
+  customRotation: boolean;
 }
 
 function RotatingCube(props: RotatingProps) {
   const {
-    setMouseDownClientX,
-    setMouseDownClientY,
-    setMouseUpClientX,
-    setMouseUpClientY,
+    mouseDownClientX,
+    mouseDownClientY,
+    mouseUpClientX,
+    mouseUpClientY,
     cameraDepth,
+    customRotation,
+    autoRotation,
   } = props;
   const cubeRef = useRef<any>(null);
 
-  const onMouseDown = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    setMouseDownClientX(e.clientX);
-    setMouseDownClientY(e.clientY);
-  };
-  const onMouseUp = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    setMouseUpClientX(e.clientX);
-    setMouseUpClientY(e.clientY);
-  };
   // // 마우스 휠 컨트롤 관련
-  // useEffect(() => {
-  //   if (cubeRef.current) {
-  //     cubeRef.current.rotation.x += "마우스ㄱㄱㄱㄱ";
-  //     cubeRef.current.rotation.y += "마우슥ㄱㄱㄱ";
-  //   }
-  // }, []);
+
+  useEffect(() => {
+    if (cubeRef.current) {
+      if (customRotation) {
+        cubeRef.current.rotation.y +=
+          (mouseUpClientX - mouseDownClientX) / 1000;
+        cubeRef.current.rotation.x +=
+          (mouseUpClientY - mouseDownClientY) / 1000;
+      }
+    }
+  }, [mouseDownClientX, mouseDownClientY, mouseUpClientX, mouseUpClientY]);
   // 자동회전 관련
   useFrame(() => {
     if (cubeRef.current) {
-      cubeRef.current.rotation.x += 0.01;
-      cubeRef.current.rotation.y += 0.01;
+      if (autoRotation) {
+        cubeRef.current.rotation.x += 0.01;
+        cubeRef.current.rotation.y += 0.01;
+      }
     }
   });
 
@@ -63,6 +69,20 @@ function MovingCubic2() {
   const [mouseDownClientY, setMouseDownClientY] = useState(0);
   const [mouseUpClientX, setMouseUpClientX] = useState(0);
   const [mouseUpClientY, setMouseUpClientY] = useState(0);
+  const [currentClientX, setCurrentClientX] = useState(0);
+  const [currentClientY, setCurrentClientY] = useState(0);
+  const [autoRotation, setAutoRotation] = useState(false);
+  const [customRotation, setCustomRotation] = useState(false);
+
+  const onMouseDown = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setMouseDownClientX(e.clientX);
+    setMouseDownClientY(e.clientY);
+  };
+  const onMouseUp = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setMouseUpClientX(e.clientX);
+    setMouseUpClientY(e.clientY);
+  };
+
   return (
     <>
       <div style={{ width: "1024px", height: "768px" }}>
@@ -71,18 +91,42 @@ function MovingCubic2() {
           setBrouserHeight={setBrowserHeight}
           setCameraDepth={setCameraDepth}
         />
-        <div>
-          {[mouseDownClientX, mouseDownClientY, mouseUpClientX, mouseUpClientY]}
-        </div>
-        <Canvas style={{ width: browserWidth, height: browserHeight }}>
+        <div>{mouseUpClientX - mouseDownClientX}</div>
+        <Checkbox checked={autoRotation} onChange={setAutoRotation}>
+          자동회전
+        </Checkbox>
+        <Checkbox checked={customRotation} onChange={setCustomRotation}>
+          수동회전
+        </Checkbox>
+        <Canvas
+          onMouseDown={onMouseDown}
+          onMouseUp={onMouseUp}
+          style={{ width: browserWidth, height: browserHeight }}
+        >
           <ambientLight />
+          <directionalLight
+            castShadow
+            position={[0, 10, 0]}
+            intensity={4}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-far={50}
+            shadow-camera-left={-100}
+            shadow-camera-right={100}
+            shadow-camera-top={100}
+            shadow-camera-bottom={-100}
+          />
           <pointLight position={[10, 10, 10]} />
           <RotatingCube
             cameraDepth={cameraDepth}
-            setMouseDownClientX={setMouseDownClientX}
-            setMouseDownClientY={setMouseDownClientY}
-            setMouseUpClientX={setMouseUpClientX}
-            setMouseUpClientY={setMouseUpClientY}
+            mouseDownClientX={mouseDownClientX}
+            mouseDownClientY={mouseDownClientY}
+            currentClientX={currentClientX}
+            currentClientY={currentClientY}
+            mouseUpClientX={mouseUpClientX}
+            mouseUpClientY={mouseUpClientY}
+            autoRotation={autoRotation}
+            customRotation={customRotation}
           />
         </Canvas>
       </div>
