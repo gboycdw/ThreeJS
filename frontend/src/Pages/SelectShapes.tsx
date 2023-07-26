7020b0ad99726ffe3f45c0e6eef01b2a4c9faec7
gomao <gboycdw@gmail.com>
2023-07-26 14:30:54 +0900

    [feat] 도형 선택가능하도록 변경, 컴포넌트 분리

---
+++ frontend/src/Pages/SelectShapes.tsx
@@ -0,0 +1,38 @@
+import { Box, Torus, Sphere } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+function SelectShapes(props: { option: boolean; shapes: string }) {
+  const { option, shapes } = props;
+  const shapesRef = useRef<any>(null);
+  useFrame(() => {
+    if (shapesRef.current) {
+      if (option) {
+        shapesRef.current.rotation.x += 0.01;
+        shapesRef.current.rotation.y += 0.01;
+      }
+    }
+  });
+
+  return (
+    <>
+      {shapes === "Cubic" && (
+        <Box ref={shapesRef} args={[1, 1, 1]}>
+          <meshStandardMaterial attach="material" color="skyblue" />
+        </Box>
+      )}
+      {shapes === "Torus" && (
+        <Torus ref={shapesRef} args={[1, 0.5, 32, 100]}>
+          <meshStandardMaterial attach="material" color="skyblue" />
+        </Torus>
+      )}
+      {shapes === "Sphere" && (
+        <Sphere ref={shapesRef} args={[1, 32, 32]}>
+          <meshStandardMaterial attach="material" color="skyblue" />
+        </Sphere>
+      )}
+    </>
+  );
+}
+
+export default SelectShapes;
