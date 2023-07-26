37c64e7b280ed9d062b1c7b7cd0492d63d2c1355
gomao <gboycdw@gmail.com>
2023-07-26 11:51:35 +0900

    [feat] 체크박스 컴포넌트 제작

---
+++ frontend/src/Components/CheckBox.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+interface CheckBoxProps {
+
+children : string,
+disabled?: boolean,
+checked: boolean,
+onChange: React.Dispatch<React.SetStateAction<boolean>>
+
+}
+
+export default function Checkbox({ children, disabled, checked, onChange }:CheckBoxProps) {
+    return (
+      <label>
+        <input
+          type="checkbox"
+          disabled={disabled}
+          checked={checked}
+          onChange={({ target: { checked } }) => onChange(checked)}
+        />
+        {children}
+      </label>
+    );
+  }
