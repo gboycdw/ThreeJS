c28331f09c5c83fdf6cffd04e73cc6d9604e16b6
gomao <gboycdw@gmail.com>
2023-08-03 16:34:16 +0900

    [feat] 3D Object Texture 테스트 추가

--- frontend/src/Pages/SelectShapes.tsx
+++ frontend/src/Pages/SelectShapes.tsx
@@ -1,15 +1,36 @@
 import { Box, Torus, Sphere, Stage } from "@react-three/drei";
 import Custom3D from "./Custom3D";
-import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
+import { useLoader, useFrame } from "@react-three/fiber";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 
-function SelectShapes(props: { option: boolean; shapes: string }) {
-  const { option, shapes } = props;
+interface movingProps {
+  turnLeft: boolean;
+  turnRight: boolean;
+  goStraight: boolean;
+  goBack: boolean;
+  goRight: boolean;
+  goLeft: boolean;
+}
+
+function SelectShapes(props: {
+  option: boolean;
+  shapes: string;
+  move: movingProps;
+  center: boolean;
+  setCenter: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const { option, shapes, move, center, setCenter } = props;
   const shapesRef = useRef<THREE.Mesh>(null);
+  const texturePath = "/texture/earth.jpg";
+  const myTexture = useLoader(TextureLoader, texturePath);
+
   useFrame(() => {
     if (shapesRef.current && option) {
-      shapesRef.current.rotation.x += 0.01;
+      if (shapes !== "Sphere") {
+        shapesRef.current.rotation.x += 0.01;
+      }
       shapesRef.current.rotation.y += 0.01;
     }
     // console.log(shapesRef);
@@ -29,10 +50,16 @@ function SelectShapes(props: { option: boolean; shapes: string }) {
       )}
       {shapes === "Sphere" && (
         <Sphere ref={shapesRef} args={[1, 32, 32]} position={[0, 1.5, 0]}>
-          <meshStandardMaterial attach="material" color="skyblue" />
+          <meshStandardMaterial
+            map={myTexture}
+            attach="material"
+            color="skyblue"
+          />
         </Sphere>
       )}
-      {shapes === "custom" && <Custom3D ref={shapesRef} />}
+      {shapes === "custom" && (
+        <Custom3D move={move} center={center} setCenter={setCenter} />
+      )}
     </>
   );
 }
