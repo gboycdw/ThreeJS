e75c8bc54cb95d1b624104b63a700afbd72d5aa9
gomao <gboycdw@gmail.com>
2023-08-06 01:38:00 +0900

    [feat] server router 설정, websoket 기본설정 추가

--- Server/index.ts
+++ Server/index.ts
@@ -5,38 +5,46 @@ import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
+// Cors
+// app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3002",
+  })
+);
 
-const corsOption = {
-  cors: {
-    origin: "http://localhost3000",
-  },
-};
-const io = new SocketIOServer(server); // 추후 server => server, corsOption 으로 변경
-app.use(cors()); // 추후 cors() => cors(corsOption.cors) 로 변경
+// Router
+const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("서버 정상 동작 중 : 메인 페이지");
+});
+app.use(router);
+
+// Server On
 const PORT = 5000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// 기본 라우트
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-// Socket.io 연결 및 채팅 로직 구현
+// Websoket
+const io = new SocketIOServer(server); // 추후 server => server, corsOption 으로 변경
 io.on("connection", (socket) => {
-  console.log("새로운 클라이언트가 연결되었습니다.");
+  console.log("새 클라이언트가 연결됨");
+
+  socket.on("join", (name, room) => {
+    console.log(name, room);
+  });
 
   // 클라이언트가 메시지를 보낼 때
   socket.on("chat message", (msg) => {
-    console.log("메시지를 받았습니다:", msg);
+    console.log("메시지 :", msg);
     io.emit("chat message", msg); // 모든 클라이언트에게 메시지 전달
   });
 
   // 클라이언트가 연결을 끊을 때
   socket.on("disconnect", () => {
-    console.log("클라이언트가 연결을 끊었습니다.");
+    console.log("클라이언트가 떠났음");
   });
 });
