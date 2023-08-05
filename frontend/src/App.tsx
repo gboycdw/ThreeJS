5d3c86a11191467d8abdb4e45be3956f766eecda
gomao <gboycdw@gmail.com>
2023-08-06 01:38:18 +0900

    [feat] 채팅기능을 테스트할 라우터 주소 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import "./Styles.css";
 import MainPage from "./Pages/MainPage";
 import MovingCubic from "./Pages/MovingCubic";
 import MovingCubic2 from "./Pages/MovingCubic2";
+import Chat from "./Components/Chat/chat";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/test1" element={<MovingCubic />} />
         <Route path="/test2" element={<MovingCubic2 />} />
+        <Route path="/chat" element={<Chat />} />
         <Route path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
