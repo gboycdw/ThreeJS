98753d9ddcad618f8042680b638178cb2eb83d66
gomao <gboycdw@gmail.com>
2023-08-06 16:45:01 +0900

    [feat] socket 연결작업 마무리

--- frontend/src/Components/Chat/chat.tsx
+++ frontend/src/Components/Chat/chat.tsx
@@ -1,35 +1,41 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
-function Chat({ location }: any) {
+function Chat() {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
   const ENDPOINT = "http://localhost:5000"; // env로 수정 필요함
-
+  let socket;
   useEffect(() => {
-    // const searchParams = new URLSearchParams(location.search);
-    // const name = searchParams.get("name");
-    // const room = searchParams.get("room");
-    console.log(name, room);
-    let socket = io(ENDPOINT);
-    console.log(io());
-    // if (name && room) {
-    //   setName(name);
-    //   setRoom(room);
+    socket = io(ENDPOINT);
 
-    //   socket.emit("join", { name, room });
-    // }
+    console.log("연결됨?", socket);
+
+    const searchParams = new URLSearchParams(window.location.search);
+    const name = searchParams.get("name");
+    const room = searchParams.get("room");
+
+    if (name && room) {
+      setName(name);
+      setRoom(room);
+
+      //   socket.emit("join", { name, room });
+    }
 
     // Clean up the socket connection on unmount
-    return () => {
-      socket.disconnect();
-    };
-  }, [
-    ENDPOINT,
-    // location.search
-  ]);
+    // return () => {
+    //     socket.disconnect();
+    // };
+  }, [ENDPOINT]);
 
-  return <div>Chat</div>;
+  return (
+    <div>
+      Chat
+      <div>유저 : {name}</div>
+      <div>방제 : {room}</div>
+    </div>
+  );
 }
 
 export default Chat;
