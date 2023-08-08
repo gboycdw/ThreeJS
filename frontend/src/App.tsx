5801adf03523d73df25f8cb00b6654164b289f1d
gomao <gboycdw@gmail.com>
2023-08-08 18:05:14 +0900

    [feat] tailwindcss를 사용하도록 루트 파일에 import 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import "./Styles.css";
 import MainPage from "./Pages/MainPage";
 import MovingCubic from "./Pages/MovingCubic";
 import MovingCubic2 from "./Pages/MovingCubic2";
