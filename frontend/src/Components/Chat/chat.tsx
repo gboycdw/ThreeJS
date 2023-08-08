c1166e793e32caf0efd965e49674ae4e3dcd68f7
gomao <gboycdw@gmail.com>
2023-08-08 18:06:47 +0900

    [feat] 채팅 기능 업그레이드 (이하 상세내용) 1. 채팅량이 많아질 경우 스크롤이 발생하도록 수정 2. 새 채팅이 생성되면 스크롤을 하단으로 갱신하도록 수정 3. 채팅방 나가기 버튼을 눌러 채팅방을 나갈 수 있도록 수정 4. 닉네임 변경 기본기능 구현(아직 검증로직 없음) 5. 임시로 사용할 styled-component 스타일 작성

--- frontend/src/Components/Chat/chat.tsx
+++ frontend/src/Components/Chat/chat.tsx
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { io, Socket } from "socket.io-client";
 import { useRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
 import { messageState } from "../../States/ChatStates";
+import { MyChat } from "../Common/chatBox";
+import styled from "styled-components";
+
 const ENDPOINT = "http://localhost:5000"; // env로 수정 필요함
-const socket: Socket = io(ENDPOINT);
+let socket: Socket = io(ENDPOINT);
 
 function Chat() {
   const [name, setName] = useState("");
+  const [newName, setNewName] = useState("");
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [chatList, setChatList] = useRecoilState(messageState);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -19,97 +25,137 @@ function Chat() {
       setName(name);
       setRoom(room);
 
-      if (socket) {
-        socket.emit("join", { name, room }, () => {
-          console.log(socket.id, "접속 완료");
-        });
-      }
+      socket.emit("join", { name, room }, (error: any) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("ㄱㅁㅇ");
+        }
+      });
     }
 
     // Clean up the socket connection on unmount
     return () => {
       socket.off();
-      // socket.disconnect();
     };
-  }, []);
+  }, [ENDPOINT]);
 
+  const scrollControl = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    if (socket) {
-      socket.on("message", (message) => {
-        setChatList([...chatList, message]);
-      });
+    socket.on("message", (message) => {
+      setChatList([...chatList, message]);
+    });
+    const scroll = scrollControl.current;
+    if (scroll) {
+      scroll.scrollTop = scroll.scrollHeight;
     }
   }, [chatList]);
 
   const sendMessage = (e: any) => {
     e.preventDefault();
     if (message) {
-      socket.emit("sendMessage", message, () => setMessage(""));
+      socket.emit("sendMessage", name, message, () => setMessage(""));
     }
     setMessage("");
   };
 
-  const searchAllUser = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-    if (socket) {
-      socket.emit("searchUser");
-    }
+  const leaveRoom = () => {
+    socket.emit("quit", name);
+    navigate("/join");
+  };
+
+  // 변경은 가능. 그런데 검증은 어떻게?
+  const changeName = () => {
+    socket.emit("change", name, newName, () => {
+      console.log("닉변성공");
+    });
+    // 변경 검증 로직 추가
+
+    setName(newName);
   };
 
   return (
     <div>
       Chat
-      <div>유저 : {name}</div>
       <div>방제 : {room}</div>
+      <div>유저 : {name}</div>
+      <div>
+        닉네임 변경
+        <input
+          value={newName}
+          onChange={(e) => {
+            setNewName(e.target.value);
+          }}
+        />
+        <button onClick={changeName}>변경</button>
+      </div>
       <button
         onClick={() => {
           setChatList([]);
         }}
       >
         대화 초기화
+      </button>{" "}
+      <br />
+      <button
+        onClick={() => {
+          console.log(chatList);
+        }}
+      >
+        채팅 리스트 타입확인
       </button>
-      <div>
-        {chatList.length > 0 &&
-          chatList.map((a, index) => {
-            return (
-              <div key={index} style={{ display: "flex" }}>
-                <div
-                  style={{
-                    border: "1px solid",
-                    padding: "4px",
-                    margin: "1px 4px",
-                  }}
-                >
-                  {a.user}
-                </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  {" "}
-                  {a.text}
-                </div>
-              </div>
-            );
-          })}
+      <button onClick={leaveRoom}>채팅방 떠나기</button>
+      {/* 채팅방 구현 */}
+      <StyledChatContainer>
+        <StyledChatBox ref={scrollControl}>
+          <MyChat chatList={chatList} name={name} />
+        </StyledChatBox>
+        <StyledInputContainer>
+          <form onSubmit={sendMessage}>
+            <StyledInputBox
+              value={message}
+              onChange={(e) => {
+                e.preventDefault();
 
-        <button onClick={searchAllUser}>ㄱㅁㅇ</button>
-      </div>
-      <div>
-        <form onSubmit={sendMessage}>
-          <input
-            style={{ border: "1px solid" }}
-            value={message}
-            onChange={(e) => {
-              e.preventDefault();
-
-              setMessage(e.target.value);
-            }}
-          />
-          <button type="submit">제출</button>
-        </form>
-      </div>
+                setMessage(e.target.value);
+              }}
+            />
+            <button type="submit">제출</button>
+          </form>
+        </StyledInputContainer>
+      </StyledChatContainer>
     </div>
   );
 }
 
 export default Chat;
+
+const StyledChatContainer = styled.div`
+  width: 30.5rem;
+  padding: 10px;
+`;
+
+const StyledChatBox = styled.div`
+  padding: 12px;
+  margin: 5.5px;
+  border-radius: 2px;
+  border: 1px solid black;
+  width: fit-content;
+  overflow-y: scroll;
+  background-color: rgb(240, 240, 240);
+`;
+
+const StyledInputContainer = styled.div`
+  background-color: skyblue;
+  display: grid;
+  padding: 7px;
+  align-items: center;
+  align-self: center;
+`;
+
+const StyledInputBox = styled.input`
+  width: 85%;
+  height: 25px;
+  margin-right: 7px;
+  border: 1px solid;
+`;
