9bf24cb26e18771fdf19fe63a98b16519efe6c3b
gomao <gboycdw@gmail.com>
2023-08-08 18:07:05 +0900

    [feat] 닉네임 변경기능 추가, socket.id대신 name 사용하도록 수정

--- Server/src/user.ts
+++ Server/src/user.ts
@@ -1,3 +1,5 @@
+import { v4 } from "uuid";
+
 export interface User {
   id: string;
   name: string;
@@ -5,38 +7,39 @@ export interface User {
 }
 
 const defaultUser: User = {
-  id: "who",
-  name: "are",
-  room: "you",
+  id: "dikfjalksdfjlk",
+  name: "anonymous",
+  room: "prison",
 };
 
 const users: User[] = [];
 
-function addUser({ id, name, room }: User) {
+function addUser({ name, room }: User) {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
+  const uid = v4();
   const existUser = users.find(
     (user: User) => user.room === room && user.name === name
   );
   if (existUser) {
     return { error: "중복된 유저이름" };
   }
-  const user = { id, name, room };
+  const user = { id: uid, name, room };
   users.push(user);
   return { user };
 }
 
-function removeUser(id: string) {
-  const index = users.findIndex((user: User) => user.id === id);
+function removeUser(name: string) {
+  const index = users.findIndex((user: User) => user.name === name);
+  console.log(index);
   if (index !== -1) {
-    console.log(`${id} 유저를 추방함`);
-    return users.splice(index, 1);
+    console.log(`${users[index].name} 유저가 떠남`);
+    return users.splice(index, 1)[0];
   }
 }
 
-function getUser(id: string) {
-  console.log(users);
-  const user = users.find((user: User) => user.id === id);
+function getUser(name: string) {
+  const user = users.find((user: User) => user.name === name);
   return user ? user : defaultUser;
 }
 
@@ -45,8 +48,12 @@ function getUsersInRoom(room: string) {
   return { users: usersInRoom };
 }
 
-function allUser(room?: string) {
-  return { users };
+function changeUserName(name: string, newname: string) {
+  const index = users.findIndex((user: User) => user.name === name);
+  if (index !== -1) {
+    users[index].name = newname;
+  }
+  return [name, newname];
 }
 
-export { addUser, removeUser, getUser, getUsersInRoom, allUser };
+export { addUser, removeUser, getUser, getUsersInRoom, changeUserName };
