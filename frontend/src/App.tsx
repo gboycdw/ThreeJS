0a9bc69204c439cd1f51f4e878c91c588aeed087
gomao <gboycdw@gmail.com>
2023-08-10 18:19:29 +0900

    [feat] 3Drender + Websoket Chatting 결합 컴포넌트 라우터 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MainPage from "./Pages/MainPage";
-import MovingCubic from "./Pages/MovingCubic";
-import MovingCubic2 from "./Pages/MovingCubic2";
+import MovingCubic from "./Components/Render/MovingCubic";
+import MovingCubic2 from "./Components/Render/MovingCubic2";
 import Chat from "./Components/Chat/chat";
 import Join from "./Components/Chat/join";
+import JoinPage from "./Pages/JoinPage";
 import { RecoilRoot } from "recoil";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/test2" element={<MovingCubic2 />} />
           <Route path="/chat" element={<Chat />} />
           <Route path="/join" element={<Join />} />
+          <Route path="/gomao/*" element={<JoinPage />} />
           <Route path="/" element={<MainPage />} />
         </Routes>
       </BrowserRouter>
