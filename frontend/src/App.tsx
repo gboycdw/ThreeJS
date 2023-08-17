97f69695aeb132e7b5fa52ea05da1af9d3f07d10
gomao <gboycdw@gmail.com>
2023-08-17 15:36:16 +0900

    feat: rrd ver6에 맞게 router 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,11 +8,20 @@ import Chat from "./Components/Chat/chat";
 import Join from "./Components/Chat/join";
 import JoinPage from "./Pages/JoinPage";
 import { RecoilRoot } from "recoil";
+import { router } from "./router";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
     <RecoilRoot>
-      <BrowserRouter>
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  );
+}
+
+export default App;
+
+/* <BrowserRouter>
         <Routes>
           <Route path="/test1" element={<MovingCubic />} />
           <Route path="/test2" element={<MovingCubic2 />} />
@@ -21,9 +30,4 @@ function App() {
           <Route path="/gomao/*" element={<JoinPage />} />
           <Route path="/" element={<MainPage />} />
         </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
-  );
-}
-
-export default App;
+      </BrowserRouter> */
---
+++ frontend/src/router.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import MainPage from "./Pages/MainPage";
+import MovingCubic from "./Components/Render/MovingCubic";
+import MovingCubic2 from "./Components/Render/MovingCubic2";
+import Chat from "./Components/Chat/chat";
+import Join from "./Components/Chat/join";
+import JoinPage from "./Pages/JoinPage";
+import checker from "./Components/loader/checker";
+import checker2 from "./Components/loader/checker2";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/test1",
+    element: <MovingCubic />,
+    action: checker2,
+    loader: checker,
+  },
+  {
+    path: "/test2",
+    element: <MovingCubic2 />,
+  },
+  {
+    path: "/chat",
+    element: <Chat />,
+  },
+  {
+    path: "/join",
+    element: <Join />,
+  },
+  {
+    path: "/gomao/*",
+    element: <JoinPage />,
+  },
+]);
