563177fa4953aababb8de2cefc35477af6fdd8d3
gomao <gboycdw@gmail.com>
2023-08-18 17:58:34 +0900

    feat: loader에서 react query가 동작하도록 수정

--- frontend/src/Components/loader/loader.tsx
+++ frontend/src/Components/loader/loader.tsx
@@ -1,32 +1,47 @@
-import axios from "axios";
+import axios, { AxiosPromise } from "axios";
+import type { QueryClient } from "@tanstack/query-core";
+import { LoaderFunctionArgs } from "react-router-dom";
 
 const DB = "http://gamezone.gomao9.com:8080/api/categories";
 
-const axiosGetData = async () => {
+const axiosGetData: () => AxiosPromise = async () => {
   const data = axios.get(DB);
   return data;
 };
 
-const query = {
+interface queryType {
+  queryKey: string[];
+  queryFn: () => AxiosPromise;
+  [key: string]: string[] | (() => AxiosPromise) | number | boolean;
+}
+const query: queryType = {
   queryKey: ["gomao"],
   queryFn: axiosGetData,
-  cacheTime: 9999,
-  staleTime: 9999,
+  cacheTime: 100,
+  staleTime: 1,
 };
 
 export const loader =
-  (queryClient: any) =>
-  async ({ request }: any) => {
+  (queryClient: QueryClient) => async (params: LoaderFunctionArgs) => {
     console.log(queryClient);
-    console.log(request);
-
-    const before = queryClient.getQueryData(query.queryKey);
-
-    if (before) {
-      console.log("ㅇㅇ");
-      return before.data;
+    console.log(params);
+    try {
+      const { data } = await queryClient.ensureQueryData(query);
+      return data;
+    } catch (e) {
+      console.log("에러임 ㅎㅎ : ", e);
+      throw new Error("ㅇㅇ");
     }
-    console.log("여기 옴?");
-    const { data } = await queryClient.fetchQuery(query);
-    return data ? data : "gomao";
+
+    // // 구버전
+    // const before: unknown = queryClient.getQueryData(query.queryKey);
+    // if (before && "data" in (before as Record<string, unknown>)) {
+    //   console.log("신선한 데이터가 이미있음ㅎ");
+
+    //   return (before as Record<string, unknown>).data;
+    // } else {
+    //   console.log("데이터가 없거나 낡았음");
+    //   const { data } = await queryClient.fetchQuery(query);
+    //   return data ? data : "gomao";
+    // }
   };
