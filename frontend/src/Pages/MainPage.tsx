4c9c03fce46b7ddb09af605f9187844cb8204445
gomao <gboycdw@gmail.com>
2023-08-23 15:25:31 +0900

    feat: 컴포넌트에서 query를 구독하도록 코드 변경

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -3,24 +3,32 @@ import { queryClient } from '../router';
 import { query } from '../Components/loader/loader';
 import MainButton from '../Components/Common/Button';
 import '../App.css';
-
+import { useQuery } from '@tanstack/react-query';
 function MainPage() {
-  const [before, setBefore] = useState<any>(queryClient.getQueryData(query.queryKey));
-  const [dd, setDd] = useState<any>(null);
-  useEffect(() => {
-    if (before && 'data' in (before as Record<string, unknown>)) {
-      setDd((before as Record<string, unknown>).data);
-    }
-    setBefore(queryClient.getQueryData(query.queryKey));
-  }, [before, dd]);
+  const bef = useQuery(query);
+  console.log(bef.data?.data);
+  // const before = queryClient.getQueryData(query.queryKey);
+  // const [before, setBefore] = useState(queryClient.getQueryData(query.queryKey));
+  // const [dd, setDd] = useState<any>(() => {
+  //   if (before && 'data' in (before as Record<string, unknown>)) {
+  //     (before as Record<string, unknown>).data;
+  //   }
+  // });
+  // useEffect(() => {
+  //   if (before && 'data' in (before as Record<string, unknown>)) {
+  //     setDd((before as Record<string, unknown>).data);
+  //   } else {
+  //     setDd(null);
+  //   }
+  // }, [before, dd]);
 
   const buttonPath = ['/test1', '/test2', '/join', '/gomao'];
   const buttonMsg = ['three.js Cubic 테스트', 'React Three Fiber 테스트', 'Websoket 테스트', '통합 테스트'];
   const buttonStyle = 'p-3 m-2 border-2 border-black bg-slate-300 rounded-lg w-35 h-35';
   return (
-    <div className="space-y-3 m-20 grid">
+    <div className="m-20 grid space-y-3">
       <div className="justify-self-center">메인페이지 입니다.</div>
-      <div className="flex  space-x-10 justify-self-center">
+      <div className="flex space-x-10 justify-self-center">
         {buttonPath.map((route: string, i: number) => {
           return (
             <MainButton key={route} route={route} style={buttonStyle}>
@@ -29,9 +37,10 @@ function MainPage() {
           );
         })}
       </div>
-      <div className="bg-slate-100 w-fit content-center text-center border-2 rounded-lg ml-9 justify-self-center">
+      <div className="ml-9 w-fit content-center justify-self-center rounded-lg border-2 bg-slate-100">
         <div>react-query 테스트 string</div>
-        <div>{typeof dd === 'object' && dd && Object.values(dd).map((a: any) => a.categoryName)}</div>
+        {bef.data?.data && <div>{Object.values(bef.data.data).map((a: any) => a.categoryName)}</div>}
+        {/* <div>{typeof dd === 'object' && dd && Object.values(dd).map((a: any) => a.categoryName)}</div> */}
       </div>
     </div>
   );
